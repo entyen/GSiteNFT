10309c3f4e159417f67638868c23ef26fb59da7e
Entyen <Admin@localhost.localdomain>
2022-03-23 19:18:09 +0300

    sync

---
+++ clientTest/src/modules/styles/globalStyles.js
@@ -0,0 +1,198 @@
+import styled from "styled-components";
+
+// Used for wrapping a page component
+export const Screen = styled.div`
+  background-color: var(--primary);
+  background-image: ${({ image }) => (image ? `url(${image})` : "none")};
+  background-size: cover;
+  background-position: center;
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+// Used for providing space between components
+export const SpacerXSmall = styled.div`
+  height: 8px;
+  width: 8px;
+`;
+
+// Used for providing space between components
+export const SpacerSmall = styled.div`
+  height: 16px;
+  width: 16px;
+`;
+
+// Used for providing space between components
+export const SpacerMedium = styled.div`
+  height: 24px;
+  width: 24px;
+`;
+
+// Used for providing space between components
+export const SpacerLarge = styled.div`
+  height: 32px;
+  width: 32px;
+`;
+
+// Used for providing a wrapper around a component
+export const Container = styled.div`
+  display: flex;
+  flex: ${({ flex }) => (flex ? flex : 0)};
+  flex-direction: ${({ fd }) => (fd ? fd : "column")};
+  justify-content: ${({ jc }) => (jc ? jc : "flex-start")};
+  align-items: ${({ ai }) => (ai ? ai : "flex-start")};
+  background-color: ${({ test }) => (test ? "pink" : "none")};
+  width: 100%;
+  background-image: ${({ image }) => (image ? `url(${image})` : "none")};
+  background-size: cover;
+  background-position: center;
+`;
+
+export const TextTitle = styled.p`
+  color: var(--primary-text);
+  font-size: 22px;
+  font-weight: 500;
+  line-height: 1.6;
+`;
+
+export const TextSubTitle = styled.p`
+  color: var(--primary-text);
+  font-size: 18px;
+  line-height: 1.6;
+`;
+
+export const TextDescription = styled.p`
+  color: var(--primary-text);
+  font-size: 16px;
+  line-height: 1.6;
+`;
+
+export const StyledClickable = styled.div`
+  :active {
+    opacity: 0.6;
+  }
+`;
+
+export const StyledButton = styled.button`
+  width: 302px;
+  height: 71px;
+  border-radius: 50px;
+  border: none;
+  background-color: var(--secondary);
+  font-size: 30px;
+  font-weight: bold;
+  color: var(--secondary-text);
+  cursor: pointer;
+  box-shadow: 0px 6px 0px -2px rgba(250, 250, 250, 0.3);
+  -webkit-box-shadow: 0px 6px 0px -2px rgba(250, 250, 250, 0.3);
+  -moz-box-shadow: 0px 6px 0px -2px rgba(250, 250, 250, 0.3);
+  :active {
+    box-shadow: none;
+    -webkit-box-shadow: none;
+    -moz-box-shadow: none;
+  }
+  :hover {
+    background: linear-gradient(307deg, #5f5bcd 24%, #ff5700 100%);
+  }
+`;
+
+export const StyledButtonWeb3 = styled.button`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 200px;
+  height: 200px;
+  border-radius: 50px;
+  border: none;
+  background-color: #1a1a1a;
+  font-size: 23px;
+  font-weight: bold;
+  color: var(--secondary-text);
+  cursor: pointer;
+  @media (max-width: 767px) {
+    justify-content: flex-start;
+    width: auto;
+    height: 88px;
+    flex-direction: row;
+  }
+  box-shadow: 0px 6px 0px -2px rgba(250, 250, 250, 0.3);
+  -webkit-box-shadow: 0px 6px 0px -2px rgba(250, 250, 250, 0.3);
+  -moz-box-shadow: 0px 6px 0px -2px rgba(250, 250, 250, 0.3);
+  :active {
+    box-shadow: none;
+    -webkit-box-shadow: none;
+    -moz-box-shadow: none;
+  }
+  :hover {
+    background: linear-gradient(307deg, #5f5bcd 24%, #ff5700 100%);
+  }
+`;
+
+export const StyledRoundButton = styled.button`
+  padding: 10px;
+  border-radius: 100%;
+  border: none;
+  background-color: var(--primary);
+  padding: 10px;
+  font-weight: bold;
+  font-size: 15px;
+  color: var(--primary-text);
+  width: 30px;
+  height: 30px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 0px 4px 0px -2px rgba(250, 250, 250, 0.3);
+  -webkit-box-shadow: 0px 4px 0px -2px rgba(250, 250, 250, 0.3);
+  -moz-box-shadow: 0px 4px 0px -2px rgba(250, 250, 250, 0.3);
+  :active {
+    box-shadow: none;
+    -webkit-box-shadow: none;
+    -moz-box-shadow: none;
+  }
+`;
+
+export const ResponsiveWrapper = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  justify-content: stretched;
+  align-items: stretched;
+  width: 100%;
+  @media (min-width: 767px) {
+    flex-direction: row;
+  }
+`;
+
+export const StyledLogo = styled.img`
+  width: 200px;
+  @media (min-width: 767px) {
+    width: 300px;
+  }
+  transition: width 0.5s;
+  transition: height 0.5s;
+`;
+
+export const StyledImg = styled.img`
+  box-shadow: 0px 5px 11px 2px rgba(0, 0, 0, 0.7);
+  border: 4px dashed var(--secondary);
+  background-color: var(--accent);
+  border-radius: 100%;
+  width: 200px;
+  @media (min-width: 900px) {
+    width: 250px;
+  }
+  @media (min-width: 1000px) {
+    width: 300px;
+  }
+  transition: width 0.5s;
+`;
+
+export const StyledLink = styled.a`
+  color: var(--secondary);
+  text-decoration: none;
+`;
